2d48c4c2b85d71139ff9258ecd112c0feb193543
Aleksandr Rogachev <aleksandr.rogachev1994@gmail.com>
2017-11-22 20:07:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solution here:
+
+function driversWithRevenueOver(arr, revenue){
+  return arr.filter(function(driver){
+    return driver.revenue > revenue;
+  });
+}
+
+function driverNamesWithRevenueOver(arr, revenue){
+  return driversWithRevenueOver(arr, revenue).map(function(driver){
+    return driver.name;
+  });
+}
+
+function exactMatch(arr, attr){
+  return arr.filter(function(driver){
+    return driver.attr === attr;
+  })
+}
